a4646371e5e6ca1142b3d3dacbf36ce282e874d7
Princekotadiya007 <princuuu77@gmail.com>
2023-03-10 21:55:07 +0530

    [prince]: check frist name

---
+++ RegexLambda/RegexLambda/RegistrationLambda.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexLambda
+{
+    public class RegistrationLambda
+    {
+        List<ModalUserClass> usermodel = new List<ModalUserClass>();
+        public Lambda()
+        {
+            ModalUserClass modelUserClass = new ModalUserClass();
+            usermodel.Add(modelUserClass);
+        }
+        public void CheckFirstNmae(string firstname)
+        {
+            if (usermodel.Any(x => x.FirstName.IsMatch(firstname)))
+            {
+                Console.WriteLine("It is A Valid Pattern");
+            }
+            else
+            {
+                Console.WriteLine("Not a VAlid Pattern");
+            }
+        }
+    }
+}
